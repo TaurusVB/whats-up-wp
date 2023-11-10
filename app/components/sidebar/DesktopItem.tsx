f5fc64413fbb0ed7262b183eac03aa8e7a0dc6ad
TaurusVB <114020229+TaurusVB@users.noreply.github.com>
2023-11-10 18:27:09 +0200

    created getUsers action, UserBox, UserList component, update styles for DesktopItem, DesktopSidebar components

--- app/components/sidebar/DesktopItem.tsx
+++ app/components/sidebar/DesktopItem.tsx
@@ -27,11 +27,21 @@ const DesktopItem: FC<IDesktopItemProps> = ({
   };
 
   return (
-    <li onClick={handleClick}>
+    <li onClick={handleClick} key={label}>
       <Link
         href={href}
         className={clsx(
-          `group flex gap-x-3 rounded-md p-3 text-sm leading-6 font-semibold text-gray-500 hover:text-black hover:bg-gray-100`,
+          ` group 
+          flex 
+          gap-x-3 
+          rounded-md 
+          p-3 
+          text-sm 
+          leading-6 
+          font-semibold 
+          text-gray-500 
+          hover:text-black 
+          hover:bg-gray-100`,
           active && "bg-gray-100 text-black"
         )}
       >
---
+++ app/users/components/UserBox.tsx
@@ -0,0 +1,52 @@
+import { FC, useCallback, useState } from "react";
+
+import { useRouter } from "next/navigation";
+
+import axios from "axios";
+
+import { User } from "@prisma/client";
+import Avatar from "@/app/components/Avatar";
+
+interface IUserBoxProps {
+  data: User;
+}
+
+const UserBox: FC<IUserBoxProps> = ({ data }) => {
+  const router = useRouter();
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleClick = useCallback(async () => {
+    setIsLoading(true);
+
+    try {
+      const conversation = await axios.post("/api/conversations", {
+        userId: data.id,
+      });
+
+      router.push(`/conversations/${conversation.data.id}`);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  }, [data.id, router]);
+
+  return (
+    <div
+      onClick={handleClick}
+      className=" w-full relative flex items-center space-x-3 bg-white p-3 hover:bg-neutral-100 rounded-lg transition cursor-pointer"
+    >
+      <Avatar user={data} />
+      <div className=" min-w-0 flex-1">
+        <div className=" focus:outline-none">
+          <div className=" flex justify-between items-center mb-1">
+            <p className="text-sm font-medium text-gray-900">{data.name}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UserBox;
