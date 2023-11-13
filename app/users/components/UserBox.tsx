9f5daf319ddc463b908c14a0b9e593f2f9774d00
TaurusVB <114020229+TaurusVB@users.noreply.github.com>
2023-11-13 18:15:06 +0200

    finish doing loading states

--- app/users/components/UserBox.tsx
+++ app/users/components/UserBox.tsx
@@ -6,6 +6,7 @@ import axios from "axios";
 
 import { User } from "@prisma/client";
 import Avatar from "@/app/components/Avatar";
+import LoadingModal from "@/app/components/LoadingModal";
 
 interface IUserBoxProps {
   data: User;
@@ -33,19 +34,22 @@ const UserBox: FC<IUserBoxProps> = ({ data }) => {
   }, [data.id, router]);
 
   return (
-    <div
-      onClick={handleClick}
-      className=" w-full relative flex items-center space-x-3 bg-white p-3 hover:bg-neutral-100 rounded-lg transition cursor-pointer"
-    >
-      <Avatar user={data} />
-      <div className=" min-w-0 flex-1">
-        <div className=" focus:outline-none">
-          <div className=" flex justify-between items-center mb-1">
-            <p className="text-sm font-medium text-gray-900">{data.name}</p>
+    <>
+      {isLoading && <LoadingModal />}
+      <div
+        onClick={handleClick}
+        className=" w-full relative flex items-center space-x-3 bg-white p-3 hover:bg-neutral-100 rounded-lg transition cursor-pointer"
+      >
+        <Avatar user={data} />
+        <div className=" min-w-0 flex-1">
+          <div className=" focus:outline-none">
+            <div className=" flex justify-between items-center mb-1">
+              <p className="text-sm font-medium text-gray-900">{data.name}</p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
